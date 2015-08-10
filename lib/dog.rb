cf4f436d67d81775d8c47893a6d15bebdf9b6562
GabiOC <oconnor.gabriele@gmail.com>
2015-08-10 15:13:45 -0400

    all done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
+
 class Dog
-  attr_accessor :leash, :plastic_bag, :walking, :vet
+  attr_accessor :leash, :plastic_bag, :walking, :vet_checkup
 
   def initialize(name, breed, owner_name)
     @name = name
@@ -24,7 +25,11 @@ class Dog
   end
 
   def owner
-    @owner
+    if @owner.class == Owner
+      @owner
+    else
+      @owner = Owner.new(@owner, self)
+    end
   end
 
   def walking?(owner)
@@ -33,21 +38,5 @@ class Dog
 
   def vet_checkup?(owner)
     owner.dog.vet_checkup
-  end
-
-  # Refactor the following methods as methods in the Owner class!
-
-  def walk(owner)
-    # REFACTOR!
-    owner.dog.leash = true
-    owner.dog.plastic_bag = true
-    owner.dog.walking = true
-  end
-
-  def vet_checkup(owner)
-    # REFACTOR!
-    owner.dog.leash = true
-    owner.dog.plastic_bag = true
-    owner.dog.vet_checkup = true
   end
 end
